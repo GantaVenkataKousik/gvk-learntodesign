54068d4a71c2dd7738818076fb9f32480b7b1065
GantaVenkatakousik <venkatakousikcse01@gmail.com>
2024-04-10 17:43:40 +0530

    commiting all the changes

--- client/src/app/components/testimonials/Reviews/Reviews.tsx
+++ client/src/app/components/testimonials/Reviews/Reviews.tsx
@@ -49,7 +49,7 @@ export default function Reviews () {
       })
       console.log(response)
 
-      setReviews((prevReviews: []) => [...prevReviews, response.data])
+      setReviews((prevReviews: Review[]) => [...prevReviews, response.data])
       // Clear input fields after successful addition
       setName('')
       setEmail('')
